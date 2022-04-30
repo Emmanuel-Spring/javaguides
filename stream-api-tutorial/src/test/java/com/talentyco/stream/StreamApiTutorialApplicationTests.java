0a096ee4e0c23b3eefa3a7d5d0575bc6edf54ca2
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2022-04-29 23:18:14 -0400

    first commit Stream

---
+++ stream-api-tutorial/src/test/java/com/talentyco/stream/StreamApiTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentyco.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
