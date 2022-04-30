b3f771f861d4fb61fff9b81aef0c6e9ede42c8d8
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2022-04-29 23:38:00 -0400

    first commit Stream

--- stream-api-tutorial/src/main/java/com/talentyco/stream/StreamApiTutorialApplication.java
+++ stream-api-tutorial/src/main/java/com/talentyco/stream/StreamApiTutorialApplication.java
@@ -1,13 +1,43 @@
 package com.talentyco.stream;
 
+import com.talentyco.stream.entities.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class StreamApiTutorialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StreamApiTutorialApplication.class, args);
+	static List<Employee> employees = new ArrayList<>();
+	static {
+		employees.add(
+				new Employee("Emmanuel", "Nieto", 6500.0, List.of("Project 1", "Project 2"))
+		);
+		employees.add(
+				new Employee("Antonio", "Santibañez", 8500.0, List.of("Project 2", "Project 3"))
+		);
+		employees.add(
+				new Employee("Magdalena", "Rodriguez", 9000.0, List.of("Project 1", "Project 4"))
+		);
+		employees.add(
+				new Employee("Nataly", "Silva", 11000.0, List.of("Project 2", "Project 6"))
+		);
+		employees.add(
+				new Employee("Martina", "Silva Nieto", 2300.0, List.of("Project 4", "Project 5"))
+		);
+		employees.add(
+				new Employee("Federico", "Echeñique", 7800.0, List.of("Project 5", "Project 2"))
+		);
 	}
 
+	public static void main(String[] args) {
+		//SpringApplication.run(StreamApiTutorialApplication.class, args);
+
+		// foreach
+	employees.stream()
+			.forEach(employee -> System.out.println(employee));
+
+	}
 }
---
+++ stream-api-tutorial/src/main/java/com/talentyco/stream/entities/Employee.java
@@ -0,0 +1,63 @@
+package com.talentyco.stream.entities;
+
+import java.util.List;
+
+public class Employee {
+
+    private String firstName;
+    private String lasttName;
+    private Double salary;
+    private List<String > projects;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lasttName, Double salary, List<String> projects) {
+        this.firstName = firstName;
+        this.lasttName = lasttName;
+        this.salary = salary;
+        this.projects = projects;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLasttName() {
+        return lasttName;
+    }
+
+    public void setLasttName(String lasttName) {
+        this.lasttName = lasttName;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public List<String> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<String> projects) {
+        this.projects = projects;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lasttName='" + lasttName + '\'' +
+                ", salary=" + salary +
+                ", projects=" + projects +
+                '}';
+    }
+}
